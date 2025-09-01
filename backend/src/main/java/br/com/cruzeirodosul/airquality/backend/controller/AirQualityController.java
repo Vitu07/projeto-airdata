c3afcc6a7fba93d48bb5c76460e92ef3fa7fbbab
Vitu07 <vitormorais20092004@gmail.com>
2025-09-01 17:08:35 -0300

    Adiciona @CrossOrigin para permissão de emergência do MVP

--- backend/src/main/java/br/com/cruzeirodosul/airquality/backend/controller/AirQualityController.java
+++ backend/src/main/java/br/com/cruzeirodosul/airquality/backend/controller/AirQualityController.java
@@ -3,6 +3,7 @@ package br.com.cruzeirodosul.airquality.backend.controller;
 import br.com.cruzeirodosul.airquality.backend.service.AirQualityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/air-quality")
+@CrossOrigin(origins = "https://web-projeto-airdata.onrender.com")
 public class AirQualityController {
 
     @Autowired
